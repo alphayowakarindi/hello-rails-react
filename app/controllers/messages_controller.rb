2bd2de7c41680935f315fcad2cac3de42902bdb0
Alphy Gacheru <alphygacheru@gmail.com>
2023-01-10 16:35:49 +0300

    Set up and fix linter errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     'jjjj'
   end
+
   def random_greeting
     @random_greeting = Message.all.sample
     render json: @random_greeting
